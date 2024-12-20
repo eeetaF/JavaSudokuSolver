64b511e5b2a1db8cc843ef155ae82cc7faf9b4f3
eeetaF <m.gubaidullin@innopolis.university>
2024-12-20 14:54:00 +0300

    little changes again

--- src/Main.java
+++ src/Main.java
@@ -184,7 +184,7 @@ public class Main {
         for (int[][] matrix : result) {
             System.out.println();
             i++;
-            System.out.println("Solution: " + i);
+            System.out.println("Solution " + i + ":");
             decodeMatrix(matrix);
             printMatrix(matrix);
         }
